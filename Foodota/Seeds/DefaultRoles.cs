3a54b40ad98c1479451c4b03e78ea3c20df65f89
anas awaad <anas.shaban.awaad@gmail.com>
2024-08-23 23:37:26 +0300

    - seed default roles - seed default users

---
+++ Foodota/Seeds/DefaultRoles.cs
@@ -0,0 +1,16 @@
+﻿using Foodota.Core.Consts;
+using Microsoft.AspNetCore.Identity;
+
+namespace Foodota.Seeds;
+
+public static class DefaultRoles
+{
+    public static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+    {
+        if(!roleManager.Roles.Any())
+        {
+            await roleManager.CreateAsync(new IdentityRole(AppRoles.Admin));
+            await roleManager.CreateAsync(new IdentityRole(AppRoles.Customer));
+        }
+    }
+}
