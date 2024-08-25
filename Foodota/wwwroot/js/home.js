baa7c23b9f16c1bea2b24bd9f62c2c1ee94c9bdc
anas awaad <anas.shaban.awaad@gmail.com>
2024-08-25 20:45:24 +0300

    fix some bugs and add alert for no restaurants

--- Foodota/wwwroot/js/home.js
+++ Foodota/wwwroot/js/home.js
@@ -3,12 +3,5 @@ function toggleMenu() {
 	$('.navigation').toggleClass('active');
 }
 
-$('.opening-hours').each(function () {
-	if ($(this).text().trim() == '') {
-		$(this).html(`<div>
-						<i class="bi bi-clock-fill" style="color:green;"></i>
-						<span class="text-danger">Closed</span>
-					  </div>`)
-	}
-})
+
 
